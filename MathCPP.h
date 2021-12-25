97f6d47c32e2a1b787d9a05e9d4b66ab4126750b
MauryDev <41791247+MauryDev@users.noreply.github.com>
2021-12-24 21:45:23 -0300

    Update MathCPP.h

--- MathCPP.h
+++ MathCPP.h
@@ -4,5 +4,11 @@
 
 namespace MathCPP
 {
-	int Evaluate(std::vector<Light::CommandResult> parameters);
+	struct VariableData
+	{
+		char* name;
+		double value;
+		VariableData(char* name, double val);
+	};
+	double Evaluate(std::vector<Light::CommandResult> parameters, std::vector<VariableData> data);
 }
